01e2bde44f66634ec84d3c3039033f2585955e4a
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-08-06 13:31:03 +0530

    get order by id

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -127,7 +127,7 @@ const getOerderByVendor=async(req,res)=>{
 const getOerderByUser=async(req,res)=>{
     try{
         const {userId}=req.params;
-        const orders = await order.find({userId:userId ,paymentStatus:'success' }).lean();
+        const orders = await order.find({userId:userId}).lean();
 
         const userIds = [...new Set(orders.map(order => order.userId))];
         const shopIds = [...new Set(orders.map(order => order.shopId))];
@@ -202,4 +202,76 @@ const getOerderByUser=async(req,res)=>{
     }
 }
 
-module.exports={createOrder,getOerderByVendor ,getOerderByUser}
+const getOrderbyId=async(req,res)=>{
+    try{
+        const { id } = req.params;
+        const orders = await order.findById(id).lean();
+        const userIds = [...new Set([orders.userId])];
+        const shopIds = [...new Set([orders.shopId])];
+        const speciesIds = [...new Set(orders.species.map(s => s.speciesId))];
+        const extensionIds = [...new Set(orders.extensions.map(e => e.extensionId))];
+
+        const [users, shops, speciesD, extensionsD] = await Promise.all([
+            user.find({ _id: { $in: userIds } }).select('name email').lean(),
+            shop.find({ _id: { $in: shopIds } }).select('shopName address').lean(),
+            species.find({ _id: { $in: speciesIds } }).select('speciesId speciesImage speciesName').lean(),
+            extension.find({ _id: { $in: extensionIds } }).select('extensionId image extensionName').lean()
+        ]);
+
+        const userMap = users.reduce((acc, user) => {
+            acc[user._id] = user;
+            return acc;
+        }, {});
+
+        const shopMap = shops.reduce((acc, shop) => {
+            acc[shop._id] = shop;
+            return acc;
+        }, {});
+
+        const speciesMap = speciesD.reduce((acc, specie) => {
+            acc[specie._id] = specie;
+            return acc;
+        }, {});
+
+        const extensionMap = extensionsD.reduce((acc, extension) => {
+            acc[extension._id] = extension;
+            return acc;
+        }, {});
+
+        // Create detailed order response
+        const detailedOrder = {
+            orderId: orders._id,
+            shopId: orders.shopId,
+            shopName: shopMap[orders.shopId]?.shopName || 'N/A',
+            shopAddress: shopMap[orders.shopId]?.address || 'N/A',
+            species: orders.species.map(species => ({
+                speciesId: species.speciesId,
+                speciesName: speciesMap[species.speciesId]?.speciesName || 'N/A',
+                speciesPrice: species.speciesPrice,
+                image: speciesMap[species.speciesId]?.speciesImage || 'N/A'
+            })),
+            extensions: orders.extensions.map(extension => ({
+                extensionId: extension.extensionId,
+                extensionName: extensionMap[extension.extensionId]?.extensionName || 'N/A',
+                price: extension.price,
+                image: extensionMap[extension.extensionId]?.image || 'N/A'
+            })),
+            orderDate: orders.orderDate,
+            status: orders.paymentStatus,
+            totalAmount: orders.totalAmount,
+            bookingId: orders.bookingId
+        };
+
+        res.json({
+            status: 200,
+            data: detailedOrder,
+            msg: "Order details"
+        });
+    }catch(error){
+        res.status(500).json({
+            status: 500,
+            error: error.message
+        });
+    }
+}
+module.exports={createOrder,getOerderByVendor ,getOerderByUser,getOrderbyId}
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {createOrder , getOerderByVendor ,getOerderByUser}=require('../controllers/orderController');
+const {createOrder , getOerderByVendor ,getOerderByUser,getOrderbyId}=require('../controllers/orderController');
 
 router.post('/order',createOrder);
 router.get('/getOrder/:vendorId',getOerderByVendor);
 router.get('/userOrder/:userId',getOerderByUser);
+router.get('/order/:id',getOrderbyId);
+
 
 
 
