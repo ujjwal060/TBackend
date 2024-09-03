3a6160be870d21b0b740ef20fe51c92704b3816e
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-09-03 18:32:56 +0530

    add change

--- controllers/contactFeedbackController.js
+++ controllers/contactFeedbackController.js
@@ -1,30 +1,30 @@
-const contact=require('../models/contactusModel');
-const feedback=require('../models/feedbackModel');
-const user=require('../models/Authmodel')
+const contact = require('../models/contactusModel');
+const feedback = require('../models/feedbackModel');
+const user = require('../models/Authmodel')
 
-const contactUs=async(req,res)=>{
-    try{
+const contactUs = async (req, res) => {
+    try {
         const { userId, name, email, subject, message } = req.body;
         const newContact = new contact({
-          userId,
-          name,
-          email,
-          message,
+            userId,
+            name,
+            email,
+            message,
         });
-    
+
         const savedContact = await newContact.save();
         res.json({
-            status:200,
-            msg:"Thanks For Contacting Us",
-            data:[]
-        })   
-    }catch(error){
+            status: 200,
+            msg: "Thanks For Contacting Us",
+            data: []
+        })
+    } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-const feedbacks=async(req,res)=>{
-    try{
+const feedbacks = async (req, res) => {
+    try {
         const { userId, rating, comments } = req.body;
 
         const newFeedback = new feedback({
@@ -34,47 +34,63 @@ const feedbacks=async(req,res)=>{
         });
         const savedFeedback = await newFeedback.save();
         res.json({
-            status:200,
-            msg:"Thanks For Your Valuable FeedBack",
-            data:[]
-        })   
-    }catch(error){
+            status: 200,
+            msg: "Thanks For Your Valuable FeedBack",
+            data: []
+        })
+    } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-const getContact=async(req,res)=>{
-    try{
-        const result = await contact.find();
+const getContact = async (req, res) => {
+    try {
+        const {read}=req.body;
+        const result = await contact.find({read:read});
         res.json({
-          status:200,
-          msg:"get all contactus",
-          data:result
-      })   
-    }catch(error){
+            status: 200,
+            msg: "get all contactus",
+            data: result
+        })
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
-const getFeedback=async(req,res)=>{
-    try{
+const getFeedback = async (req, res) => {
+    try {
         const result = await feedback.find();
         const feedbacks = await Promise.all(result.map(async (feedbackItem) => {
             const users = await user.findById(feedbackItem.userId);
-            
+
             return {
                 ...feedbackItem._doc,
                 username: users.name,
             };
         }));
         res.json({
-          status:200,
-          msg:"get all feedback",
-          data:feedbacks
-      })   
-    }catch(error){
+            status: 200,
+            msg: "get all feedback",
+            data: feedbacks
+        })
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
-module.exports = {contactUs,feedbacks,getContact,getFeedback}
+const updateContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { read } = req.body;
+        const result=await contact.findByIdAndUpdate(id,{read});
+        res.json({
+            status: 200,
+            msg: "readed",
+            data: []
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { contactUs, feedbacks, getContact, getFeedback, updateContact }
