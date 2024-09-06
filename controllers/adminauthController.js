470de108c7795340698b285d3a419a3500edbd57
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-09-06 16:47:10 +0530

    add change

--- controllers/adminauthController.js
+++ controllers/adminauthController.js
@@ -1,4 +1,6 @@
-const User = require('../models/adminauthModel');
+const User = require('../models/Authmodel');
+const Order=require('../models/orderModel');
+const Subscription=require('../models/subscriptionModel');
 const jwt = require('jsonwebtoken');
 const { notification } = require('./notification')
 
@@ -39,11 +41,22 @@ const bulkNotification = async (req, res) => {
   try {
     const { title, body, role } = req.body;
     const users = await User.find({ role });
-    const notificationsPromises = users.map(user => {
-      const userId=user._id;
-      const deviceToken=user.deviceToken;
-      return notification(userId, title, body, deviceToken);
-    });
+    const notificationsPromises = [];
+    for (const user of users) {
+      const userId = user._id;
+      const deviceToken = user.deviceToken;
+    
+      if (deviceToken) {
+        const promise = notification(userId, title, body, deviceToken)
+          .catch(error => {
+            console.error(error);
+          });
+    
+        notificationsPromises.push(promise);
+      } else {
+        console.warn(`User with ID ${userId} does not have a deviceToken.`);
+      }
+    }
     await Promise.all(notificationsPromises);
     res.status(200).json({
       status: 200,
@@ -57,4 +70,30 @@ const bulkNotification = async (req, res) => {
   }
 }
 
-module.exports = { login, logout, bulkNotification };
+const dashboard=async(req,res)=>{
+  try {
+    const usersCount = await User.countDocuments({role:'user'}); 
+    const vendorCount = await User.countDocuments({role:'vendor'});  
+
+    const orderRevenue = await Order.aggregate([
+      { $group: { _id: null, totalAmount: { $sum: '$totalAmount' } } }
+    ]);
+
+    const subscriptionRevenue = await Subscription.aggregate([
+      { $group: { _id: null, totalAmount: { $sum: '$price' } } }
+    ]);
+
+    res.json({
+      users: usersCount,
+      revenue: {
+        orderAmount: orderRevenue[0]?.totalAmount || 0,
+        subscriptionAmount: subscriptionRevenue[0]?.totalAmount || 0 
+      },
+      vendor:vendorCount
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+module.exports = { login, logout, bulkNotification,dashboard};
--- routes/adminauthRoute.js
+++ routes/adminauthRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {login, logout,bulkNotification } = require('../controllers/adminauthController');
+const {login, logout,bulkNotification,dashboard} = require('../controllers/adminauthController');
 // const { verifyToken } = require('../middleware/verifyToken');
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/sendN', bulkNotification);
+router.get('/dashboard', dashboard);
 
 module.exports = router;
